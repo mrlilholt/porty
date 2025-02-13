460b0d4efd38fe656ab80b97c1abf8d7a8c5899f
drexelcybercamp <86355978+drexelcybercamp@users.noreply.github.com>
2025-02-12 19:48:16 -0500

    Update TechStack logo sizing

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -3,14 +3,14 @@ import './TechStack.css';
 
 const techStackData = [
   { name: "React", logo: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" },
-  { name: "Node.js", logo: "https://nodejs.org/static/images/logo.svg" },
-  { name: "HTML5", logo: "https://upload.wikimedia.org/wikipedia/commons/b/bf/HTML5_logo.svg" },
+  { name: "Node.js", logo: "https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg" },
+  { name: "HTML5", logo: "https://en.wikipedia.org/wiki/HTML5#/media/File:HTML5_logo_and_wordmark.svg" },
   { name: "CSS3", logo: "https://upload.wikimedia.org/wikipedia/commons/6/62/CSS3_logo.svg" },
   { name: "JavaScript", logo: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" },
   { name: "Git", logo: "https://upload.wikimedia.org/wikipedia/commons/e/e0/Git-logo.svg" },
-  { name: "EmailJS", logo: "https://via.placeholder.com/50?text=EmailJS" },
+  { name: "EmailJS", logo: "https://www.emailjs.com/logo.png" },
   { name: "Visual Studio Code", logo: "https://upload.wikimedia.org/wikipedia/commons/9/9a/Visual_Studio_Code_1.35_icon.svg" },
-  { name: "Vite", logo: "https://raw.githubusercontent.com/vitejs/vite/main/packages/playground/public/vite.svg?sanitize=true" }
+  { name: "Vite", logo: "https://upload.wikimedia.org/wikipedia/commons/f/f1/Vitejs-logo.svg" }
 ];
 
 const TechStack = () => {
