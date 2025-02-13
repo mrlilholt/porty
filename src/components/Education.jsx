44fb430b915b78ef25ee217a45f8c7571d64edb1
drexelcybercamp <86355978+drexelcybercamp@users.noreply.github.com>
2025-02-12 20:07:19 -0500

    Update Education component with school homepage links

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -2,27 +2,29 @@ import React from 'react';
 import './Education.css';
 
 const educationData = [
-    {
-        degree: "Project Based Learning Certificate",
-        school:
-          "University of Pennsylvania",
-        description: "2019-2020",
-      },
-    {
+  {
+    degree: "Project Based Learning Certificate",
+    school: "University of Pennsylvania",
+    description: "2019-2020",
+    url: "https://www.upenn.edu",
+  },
+  {
     degree: "MA Environmental Education",
     school: "Arcadia University",
     description: "",
+    url: "https://www.arcadia.edu",
   },
   {
     degree: "BS Business Administration and Food Marketing",
     school: "Saint Joseph's University",
     description: "",
+    url: "https://www.sju.edu",
   },
   {
     degree: "State Certifications: 2010 - present",
-    school:
-      "State of Pennsylvania – Environmental Education (K-12) & State of Pennsylvania – General Science (7-12)",
+    school: "State of Pennsylvania – Environmental Education (K-12) & State of Pennsylvania – General Science (7-12)",
     description: "",
+    url: "https://www.education.pa.gov",
   },
 ];
 
@@ -40,7 +42,7 @@ const Education = () => {
                 {item.description && (
                   <p className="item-description">{item.description}</p>
                 )}
-                <a href="#" className="item-link">
+                <a href={item.url} className="item-link" target="_blank" rel="noreferrer">
                   {item.school}
                 </a>
               </div>
