e5ee71dff18b69fb177e7067bd31c6b457d79054
drexelcybercamp <86355978+drexelcybercamp@users.noreply.github.com>
2025-02-12 19:53:18 -0500

    Update TechStack with new logo links

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -4,7 +4,7 @@ import './TechStack.css';
 const techStackData = [
   { name: "React", logo: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" },
   { name: "Node.js", logo: "https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg" },
-  { name: "HTML5", logo: "https://en.wikipedia.org/wiki/HTML5#/media/File:HTML5_logo_and_wordmark.svg" },
+  { name: "HTML5", logo: "https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg" },
   { name: "CSS3", logo: "https://upload.wikimedia.org/wikipedia/commons/6/62/CSS3_logo.svg" },
   { name: "JavaScript", logo: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" },
   { name: "Git", logo: "https://upload.wikimedia.org/wikipedia/commons/e/e0/Git-logo.svg" },
